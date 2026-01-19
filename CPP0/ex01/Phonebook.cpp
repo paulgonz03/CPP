eb7036b67f0684ac4e8a6bcaad445a8bc9ef3b4a
Paula Gonzalez Serrano <paulgonz@c1r13s3.42madrid.com>
2026-01-19 12:55:13 +0100

    CPP0

--- CPP0/ex01/Phonebook.cpp
+++ CPP0/ex01/Phonebook.cpp
@@ -52,9 +52,9 @@ void Phonebook::addContact()
         return;
     }
 
+    count = count % 8;
     Contact contact(firstName, lastName, nickname, phoneNumber, darkestSecret);
     this->contacts[count] = contact;
-    count = count % 8;
     count++;
 
     std::cout << std::endl
